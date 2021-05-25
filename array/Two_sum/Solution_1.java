f37480a8f214b00fb3555d084591f547450c0ae7
therealkundanpatil <kp28021996@gmail.com>
2021-05-25 00:11:32 -0400

    Formatted according to convention

--- array/Two_sum/Solution_1.java
+++ array/Two_sum/Solution_1.java
@@ -3,34 +3,34 @@ package array.Two_sum;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution_1{
-	
+public class Solution_1 {
+
 	public static int[] computeTwoSum(int[] nums, int target) {
-		int[] result = {0,0};
-		
+		int[] result = { 0, 0 };
+
 		Map<Integer, Integer> map = new HashMap<>();
-		
-		for(int i=0;i<nums.length;i++) {
+
+		for (int i = 0; i < nums.length; i++) {
 			int complement = target - nums[i];
-			if(map.containsKey(complement)) {
+			if (map.containsKey(complement)) {
 				result[0] = i;
 				result[1] = map.get(complement);
-			}
-			else {
+			} else {
 				map.put(nums[i], i);
 			}
 		}
-		
+
 		return result;
-		
+
 	}
+
 	public static void main(String[] args) {
 		int[] nums = { 2, 7, 11, 15 };
 		int target = 9;
-		
+
 		int[] answer = computeTwoSum(nums, target);
-		for(int i=0;i<answer.length;i++) {
-			System.out.print(answer[i]+" ");
+		for (int i = 0; i < answer.length; i++) {
+			System.out.print(answer[i] + " ");
 		}
 	}
 }
